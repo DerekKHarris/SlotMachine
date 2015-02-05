2ed57851e651329fd5434c5bcd14100a97355e4c
DerekKHarris <DerekKyleHarris@gmail.com>
2015-02-04 22:13:35 -0800

    Initial Commit
    Slot Machine

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,15 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Derek Harris on 2/4/15.
+//  Copyright (c) 2015 Derek Harris. All rights reserved.
+//
+
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+}
